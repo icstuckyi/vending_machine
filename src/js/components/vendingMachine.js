29c275e106211f8d97018a75359318b8b89719ca
Seulki Yun <icstuckyi@gmail.com>
2023-01-14 14:48:58 +0900

    feat: 자판기 메뉴 기능 구현

--- src/js/components/vendingMachine.js
+++ src/js/components/vendingMachine.js
@@ -38,8 +38,10 @@ class VendingMachine {
       const balanceVal = parseInt(this.balance.textContent.replaceAll(",", ""));
 
       if (inputCost) {
+        // 입금액이 소지금보다 적다면
         if (inputCost <= myMoneyVal && inputCost > 0) {
           this.myMoney.textContent =
+            //Intl.NumberFormat : 언어에 맞는 숫자 서식을 문자열로 반환합니다. IE11 부터 지원
             new Intl.NumberFormat().format(myMoneyVal - inputCost) + " 원";
           this.balance.textContent =
             new Intl.NumberFormat().format(
@@ -63,6 +65,59 @@ class VendingMachine {
         this.balance.textContent = "원";
       }
     });
+
+    // 자판기 메뉴 기능
+    const btnCola = this.itemList.querySelectorAll("button");
+
+    btnCola.forEach((item) => {
+      item.addEventListener("click", (event) => {
+        const targetEl = event.currentTarget;
+        const balanceVal = parseInt(
+          this.balance.textContent.replaceAll(",", "")
+        );
+        let isStaged = false; // 이미 선택되었는가?
+        let targetElPrice = parseInt(targetEl.dataset.price);
+        const stagedListItem = this.stagedList.querySelectorAll("li");
+
+        // 입금된 금액이 음료수 값보다 많거나 같을 경우(구매 가능)
+        if (balanceVal >= targetElPrice) {
+          this.balance.textContent =
+            new Intl.NumberFormat().format(balanceVal - targetElPrice) + " 원";
+
+          // forEach 문을 사용할 경우 반복의 종료가 불가능하다 (return, break 작동안함). 모든 원소를 순환할 필요가 없다면 비효율적.
+          // 클릭한 음료수가 내가 이미 선택한 아이템인지 탐색
+          for (const item of stagedListItem) {
+            // 내가 클릭한 상품과 내가 담은 상품이 같을 경우
+            if (item.dataset.item === targetEl.dataset.item) {
+              item.querySelector(".num-counter").textContent++;
+              isStaged = true;
+              break;
+            }
+          }
+
+          // 해당 아이템을 처음 선택했을 경우
+          if (!isStaged) {
+            this.stagedItemGenerator(targetEl);
+          }
+          // 콜라의 갯수 줄이기
+          targetEl.dataset.count--;
+
+          // 상품이 소진되면 품절 표시
+          if (parseInt(targetEl.dataset.count) === 0) {
+            targetEl.parentElement.classList.add("sold-out");
+            const warning = document.createElement("em");
+            warning.textContent = "해당 상품은 품절입니다.";
+            warning.classList.add(".ir");
+            // em 요소를 button 요소 앞으로 배치
+            targetEl.parentElement.insertBefore(warning, targetEl);
+          }
+        } else {
+          alert("잔액이 부족합니다. 돈을 입금해주세요.");
+        }
+      });
+    });
+
+    // 획득 버튼 기능
   }
 }
 
