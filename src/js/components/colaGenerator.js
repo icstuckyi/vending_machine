72f319d50a53a6782e594d60963291b058542b84
Seulki Yun <icstuckyi@gmail.com>
2023-01-17 21:10:13 +0900

    colaGenerator 오타 수정 및 스타일 수정

--- src/js/components/colaGenerator.js
+++ src/js/components/colaGenerator.js
@@ -25,10 +25,10 @@ class ColaGenerator {
     data.forEach((el) => {
       const item = document.createElement("li");
       const itemTemplate = `
-        <button type="button" class="btn-item" data-item="${el.name} data-count="${el.count} data-price="${el.cost}" data-img="${el.img}">
+        <button type="button" class="btn-item" data-item="${el.name}" data-count="${el.count}" data-price="${el.cost}" data-img="${el.img}">
         <img src="src/images/${el.img}" alt="" class="img-item" />
-        <strong class-"tit-item">${el.name}</strong>
-        <span class-"txt-price">${el.cost}원</span>
+        <strong class="tit-item">${el.name}</strong>
+        <span class="txt-price">${el.cost}원</span>
         </button>
       `;
       item.innerHTML = itemTemplate;
